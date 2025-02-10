9413b76d2439626c11047a593cf59e9cf731fe29
babylon-detective <epignosis@gmail.com>
2025-02-10 11:53:16 -0400

    Initial commit

---
+++ src/game.js
@@ -0,0 +1,24 @@
+import Phaser from "phaser";
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload,
+        create,
+        update
+    }
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image('sky', 'https://labs.phaser.io/assets/skies/space3.png');
+}
+
+function create() {
+    this.add.image(400, 300, 'sky');
+}
+
+function update() {}
